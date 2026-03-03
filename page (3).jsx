65e89c7c9cb78614a7f633a74afee794b6120ec5
Stacy Pearce <pearce.stacy8@googlemail.com>
2026-03-03 21:32:52 +0000

    Add files via upload

---
+++ page (3).jsx
@@ -0,0 +1,8 @@
+export default function CreateProfilePage() {
+  return (
+    <main style={{ padding: 40, minHeight: "100vh" }}>
+      <h1>CREATE PROFILE</h1>
+      <p>This is the create profile page.</p>
+    </main>
+  );
+}
